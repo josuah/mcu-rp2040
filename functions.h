3c1bb986e8f6cff614d0213d39f8fdcdbbf0321b
Josuah Demangeon <me@josuah.net>
2022-03-12 14:15:06 +0100

    oh yeah babe

--- functions.h
+++ functions.h
@@ -1,10 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-#define MASK32(o)		(o == 31 ? 0xFFFFFFFF : (1u << (o + 1)) - 1)
-#define MASK(fld)		(MASK32(fld##_Msb) ^ MASK32(fld##_Lsb - 1))
-#define BIT(fld)		(1u << (fld))
-#define BITS(fld, val)		((val) << fld##_Lsb)
-#define FIELD(reg, fld)		(((reg) & MASK32(fld##_Msb)) >> fld##_Lsb)
+
+#define FIELD(reg, fld)		(((reg) & fld##_Msk) >> fld##_Pos)
 
 /* halt the execution */
 void __stop_program(void);
